e7634b401537f922832ebc8ce96743661bf7390c
Carms Ng <contact@carms-ng.com>
2021-08-31 13:44:15 -0400

    [WIP] Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,82 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
+import background from "../assets/images/background.jpg"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
+
+const HeroStyles = styled.section`
+  height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-position: 70% 50%;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  h1 {
+    font-size: 4rem;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0.2) 55%, rgba(255, 198, 0, 0.7) 35%);
+    padding: 1rem 1rem 0 1rem;
+    line-height: 1;
+  }
+  p {
+    font-size: 2rem;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 1rem 0;
+    padding: 0 1rem;
+  }
+`
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Carms Ng | Full Stack Developer" />
+    {/* HERO */}
+    <HeroStyles>
+      <h1>Hi folks! Carms here!</h1>
+      <p>I'm a Full Stack Developer living in Tiohtiá:ke aka Montréal.</p>
+      <a className="btn" href="#about">What I do</a>
+    </HeroStyles>
+
+    {/* ABOUT */}
+    <div>
+      <div>
+        <h2>About</h2>
+        <p>I’m passionate about building impactful applications to improve lives around me. I’m a versatile Full Stack RoR Developer. I’m climate and social justice-minded, communicative, fun, curious, adaptive and always up for a new challenge.</p>
+        {/* TODO: Wire PDF */}
+        <a href="#resume">Resume</a>
+      </div>
+
+      <div>
+        <div>
+          <h3>Favorite Tools</h3>
+          {/* TODO: Create ICONS */}
+
+        </div>
+        <div>
+          <h3>guiding values</h3>
+          {/* TODO: Create ICONS */}
+        </div>
+      </div>
+    </div>
+
+    {/* Portfolio */}
+    <div>
+      <h2>portfolio</h2>
+    </div>
+
+    {/* HOBBY */}
+    <div>
+      <h2>hobby</h2>
+    </div>
+
+    {/* CONTACT */}
+    <div>
+      <h2>contact</h2>
+    </div>
   </Layout>
 )
 
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -22,23 +22,66 @@ const GlobalStyles = createGlobalStyle`
   }
   *, *:before, *:after {
     box-sizing: inherit;
+    margin: 0;
   }
   body {
-    font-family: 'Overpass', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Overpass, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
     line-height: 1.6;
   }
   h1, h2, h3, h4, h5, h6, a, button {
-    font-family: 'Teko', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Teko, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    line-height: 1.2;
+    font-weight: 300;
+  }
+
+  h2, h3 {
+    width: fit-content;
+    font-size: 3rem;
+    text-transform: lowercase;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 55%, rgba(255, 198, 0, 0.7) 35%);
+    line-height: 90%;
+    padding: 0 10px;
+    margin-bottom: 1rem;
+    margin: 0 auto 1rem auto;
+    width: max-content;
+  }
+  h3 {
+    font-size: 2rem;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 55%, var(--light-blue) 35%);
+    margin: 30px auto;
+  }
+  h4 {
+    font-size: 3rem;
+  }
+  p {
+    font-family: Overpass, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.5rem;
   }
   a {
     text-decoration: none;
     color: var(--black);
   }
-  a:hover {
-    text-decoration: underline;
+  .container {
+    max-width: 1280px;
+    padding: 0 20px;
+    margin: 0 auto;
+  }
+  button, .btn {
+    text-transform: uppercase;
+    font-size: 2rem;
+    padding: 6px 20px 3px 20px;
+    border: 3px solid var(--dark);
+    transition: all 0.5s;
+    background: rgba(255, 255, 255, 0.3);
+    cursor: pointer;
+  }
+  button:hover, .btn:hover {
+    background-color: rgba(255, 198, 0, 0.5);
+    border-color: rgba(255, 198, 0, 0.7);
+    ;
   }
 `;
 
