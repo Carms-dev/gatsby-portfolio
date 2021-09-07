19075fe91d95458eb57767520fda379691cac78a
Carms Ng <contact@carms-ng.com>
2021-09-06 21:49:44 -0400

    [FIX] header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,45 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import styled from "styled-components"
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
 
 const HeaderStyles = styled.header`
   height: 80px;
   position: fixed;
   top: 0;
   left: 0;
+  padding: 1rem;
 
   a {
     display: flex;
     align-items: center;
   }
-  img {
-    height: 80px;
-    padding: 1rem;
-    object-fit: contain;
-  }
-`
+`;
 
 const Header = () => (
   <HeaderStyles>
     <Link to="/">
-      <StaticImage src="../assets/images/logo.png" alt="Carms Ng Logo" />
+      <StaticImage
+        src="../assets/images/logo.png"
+        alt="Carms Ng Logo"
+        imgStyle={{ objectFit: 'contain', width: '100%', height: '100%' }}
+        style={{ width: '70px', height: '70px', marginRight: '1rem' }}
+      />
       <div>
         <h4>Carms Ng</h4>
         <p>Full Stack Developer</p>
       </div>
     </Link>
   </HeaderStyles>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: '',
+};
 
-export default Header
+export default Header;
