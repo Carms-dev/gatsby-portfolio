ef500b227b9c62be79d3375ff3c0e88318b3d0cc
Carms Ng <contact@carms-ng.com>
2021-09-09 02:12:35 -0400

    [ADD] Menu Component and updated Header button to toggle menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,41 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
+import { Icon } from '@iconify/react';
+import Menu from './Menu';
 
-const HeaderStyles = styled.header`
-  height: 80px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding: 1rem;
-  a {
-    display: flex;
-    align-items: center;
-  }
-  .header-text {
-    display: none;
-  }
-  h4 {
-    font-size: 3.5rem;
-  }
-  p {
-    font-size: 2rem;
-  }
-  @media (min-width: 640px) {
-    .header-text {
-      display: block;
-    }
-  }
-`;
+function Header({
+  isMenuOpen, setMenuOpen, pausedRef, sectionRefs,
+}) {
+  const toggleMenu = () => { setMenuOpen(!isMenuOpen); };
 
-const Header = () => (
-  <HeaderStyles>
-    <Link to="/">
-      <StaticImage
-        src="../assets/images/logo.png"
-        alt="Carms Ng Logo"
-        imgStyle={{ objectFit: 'contain', width: '100%', height: '100%' }}
-        style={{ width: '70px', height: '70px', marginRight: '1rem' }}
-      />
-      <div className="header-text">
-        <h4>Carms Ng</h4>
-        <p>Full Stack Developer</p>
-      </div>
-    </Link>
-  </HeaderStyles>
-);
+  return (
+    <HeaderStyles>
+      <Link
+        to="/"
+        className="btn-logo"
+      >
+        <StaticImage
+          src="../assets/images/logo.png"
+          alt="Carms Ng Logo"
+          imgStyle={{ objectFit: 'contain', width: '100%', height: '100%' }}
+          style={{ width: '70px', height: '70px', marginRight: '1rem' }}
+        />
+        <div className="header-text">
+          <h4>Carms Ng</h4>
+          <p>Full Stack Developer</p>
+        </div>
+      </Link>
+      <button className="btn-menu" type="button" onClick={toggleMenu}>
+        <Icon icon={`${isMenuOpen ? 'akar-icons:cross' : 'mdi:hamburger'}`} />
+      </button>
+      {/* <div className="nav" /> */}
+      <Menu isMenuOpen={isMenuOpen} pausedRef={pausedRef} sectionRefs={sectionRefs} />
+    </HeaderStyles>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -54,5 +44,54 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: '',
 };
+
+const HeaderStyles = styled.header`
+  z-index: 10;
+  height: 80px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: grid;
+  grid-auto-flow: column;
+  align-self: flex-start;
+
+  .btn-logo {
+    display: grid;
+    grid-auto-flow: column;
+    place-content: baseline;
+  }
+  .header-text {
+    display: none;
+    h4 {
+      font-size: 4rem;
+    }
+  }
+  .btn-menu {
+    padding: 1rem;
+    justify-self: flex-end;
+    align-self: flex-start;
+    z-index: 12;
+  }
+  .btn-menu svg {
+    font-size: 3.5rem;
+    color: var(--dark);
+  }
+  .btn-logo, .btn-menu {
+    padding: 1rem;
+  }
+
+  @media (min-width: 640px) {
+    .header-text {
+      display: block;
+    }
+    .btn-menu svg {
+      font-size: 4.5rem;
+    }
+    .btn-logo, .btn-menu {
+      padding: 2rem;
+    }
+  }
+`;
 
 export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,114 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { graphql, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
+import SocialIcons from './SocialIcons';
+
+function Menu({ isMenuOpen, pausedRef, sectionRefs }) {
+  const { resume } = useStaticQuery(graphql`
+    query {
+      resume: file(extension: {eq: "pdf"}) {
+        publicURL
+      }
+    }
+  `);
+
+  const menuItems = [
+    {
+      label: 'Home',
+      ref: '0',
+      type: 'button',
+    },
+    {
+      label: 'About',
+      ref: '1',
+      type: 'button',
+    },
+    {
+      label: 'Portfolio',
+      ref: '2',
+      type: 'button',
+    },
+    {
+      label: 'Resume',
+      ref: resume.publicURL,
+      type: 'anchor',
+    },
+    {
+      label: 'Contact',
+      ref: '3',
+      type: 'button',
+    },
+  ];
+
+  const handleClick = (ev) => {
+    // Pause observer effect
+    pausedRef.current = true;
+
+    const refIndex = parseInt(ev.target.dataset.index, 10);
+    const currentRef = sectionRefs.current[refIndex];
+    const top = currentRef.offsetTop;
+    window.scrollTo({ top, behavior: 'smooth' });
+
+    // Un-pause observer effect
+    setTimeout(() => {
+      pausedRef.current = false;
+    }, 2000);
+  };
+
+  return (
+    <MenuStyles isMenuOpen={isMenuOpen}>
+      {menuItems.map(({ label, ref, type }) => {
+        if (type === 'anchor') return <a key={ref} href={ref}>{label}</a>;
+
+        return (
+          <button type="button" onClick={handleClick} data-index={ref}>{label}</button>
+        );
+      })}
+
+      <SocialIcons />
+    </MenuStyles>
+  );
+}
+
+const MenuStyles = styled.div`
+  z-index: 11;
+  box-shadow: 0px 0px 20px rgba(0,0,0,0.2);
+  position: fixed;
+  top: 0;
+
+  display: grid;
+  transition: all 1s ease;
+  justify-items: flex-start;
+  transform: ${(props) => (props.isMenuOpen ? '' : 'translate(0, -100%)')};
+  background: var(--light);
+  width: 100%;
+  grid-gap: 1rem;
+  padding: 2rem;
+
+  > button, > a {
+    font-size: 2.5rem;
+    text-align: left;
+  }
+
+  > button:hover, > a:hover {
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 55%, rgba(255, 198, 0, 0.7) 35%);
+  }
+
+  @media (min-width: 640px) {
+    right: 0;
+    bottom: 0;
+    width: auto;
+    transform: ${(props) => (props.isMenuOpen ? '' : 'translate(100%, 0)')};
+    justify-items: stretch;
+    place-content: center;
+    padding: 4rem;
+    grid-gap: 2rem;
+
+    > button, > a {
+      font-size: 4rem;
+    }
+  }
+`;
+
+export default Menu;
