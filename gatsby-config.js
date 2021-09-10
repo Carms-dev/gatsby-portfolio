b7e907d801712ce58f45f85255fca7fd70962ecb
Carms Ng <contact@carms-ng.com>
2021-09-10 17:21:03 -0400

    [ADD] og metatags image and url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: `Carms Ng | Full Stack Developer`,
-    description: `I’m passionate about building impactful applications to improve lives around me. I’m a versatile Full Stack RoR Developer. I’m climate and social justice-minded, communicative, fun, curious, adaptive and always up for a new challenge.`,
-    author: `@Carms-dev`,
-    siteUrl: `https://www.carms-ng.com/`,
+    title: 'Carms Ng | Full Stack Developer',
+    description: 'I’m passionate about building impactful applications to improve lives around me. I’m a versatile Full Stack RoR Developer. I’m climate and social justice-minded, communicative, fun, curious, adaptive and always up for a new challenge.',
+    author: '@Carms-dev',
+    siteUrl: 'https://www.carms-ng.com/',
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/assets/images`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `pdfs`,
+        name: 'pdfs',
         path: `${__dirname}/src/assets/pdfs/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: `src/assets/images/logo.png`, // This path is relative to the root of the site.
+        icon: 'src/assets/images/logo.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    'gatsby-plugin-gatsby-cloud',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
